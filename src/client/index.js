c5ca9237990803b18dc3d522d89cef92e68206fd
Shatha Makhlouf <shada_m2006@yahoo.com>
2021-01-02 16:26:06 +0200

    Initial commit

---
+++ src/client/index.js
@@ -0,0 +1,24 @@
+import { checkForInput } from './js/inputChecker'
+import { handleSubmit } from './js/formHandler'
+import { resultImg } from './js/outputImgResult'
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import boardP from './images/boardP.jpg';
+import boardN from './images/boardN.jpg';
+import boardNeg from './images/boardNeg.jpg';
+import logo from './images/logo.png';
+let logoImg = document.getElementById('logo');
+logoImg.src = logo;
+console.log("CHANGE!!");
+
+export {
+    resultImg,
+    checkForInput,
+    handleSubmit,
+    boardP,
+    boardN,
+    boardNeg
+}
